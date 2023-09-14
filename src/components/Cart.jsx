67b7612fd243f32612ee8d5266c5ab1a92c110d6
jolinhappy <jolinhappy1990@hotmail.com>
2023-09-14 22:58:53 +0800

    feat: get cartItemList by useSelector

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,17 @@
 import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
 export default function Cart() {
+  const { cartItemList } = useSelector(state => state.shoppingCartReducer);
   return (
     <div className="cart">
       <h2>Shopping Cart</h2>
       <div className="cart-item-container">
-        <CartItem />
+        {
+          cartItemList.map((cartItem) => (
+            <CartItem cartItem={cartItem} key={cartItem.id} />
+          ))
+        }
       </div>
       <div className="total-amount">
         Total Amount:  
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,8 @@
-export default function CartItem() {
+export default function CartItem({ cartItem }) {
   return (
     <div className="cart-item-content-container">
-      <p className="cart-item-name">Apple</p>
-      <input className="cart-item-amount" type="number" min="1" max="99" />
+      <p className="cart-item-name">{cartItem.title}</p>
+      <input className="cart-item-amount" type="number" min="1" max="99" value={cartItem.amount} key={cartItem.title} />
       <button className="cart-item-delete-btn">delete</button>
     </div>
   )
